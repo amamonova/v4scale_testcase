b21057af7fe319b9be76398608dff7c5205f9edd
Anastasiya Mamonova <nastia.mamonova@yandex.ru>
2020-03-17 13:05:37 +0300

    tests added. script modified

--- script.py
+++ script.py
@@ -24,3 +24,75 @@ def get_soup(html):
     return BeautifulSoup(html, 'html.parser')
 
 
+def clean_price_target(inp_str):
+    """
+    This function preprocess raw price string to float number.
+
+    :param inp_str: The string with $ sign and/or with '➝' symbol
+    :return: float number of price
+    """
+    if '➝' in inp_str and len(inp_str) > 1:
+        return float(inp_str.split('➝')[1].strip()[1:])
+    elif inp_str == '➝':
+        return 0.0
+    else:
+        return float(inp_str[1:])
+
+
+def preprocess_rating_column(inp_str):
+    if type(inp_str) == str and '➝' in inp_str:
+        return inp_str.split('➝')[1].strip()
+    return inp_str
+
+
+def get_metrics(dataframe, metric_type, ratings='Sell Hold Buy', freq='D'):
+    res = {}
+    for t in ratings.split(' '):
+
+        if metric_type == 'ins':
+            series = dataframe[dataframe.Rating == t].set_index('Date')\
+                .groupby(pd.Grouper(freq=freq))['Price Target'].count()
+        elif metric_type == 'amt':
+            series = dataframe[dataframe.Rating == t].set_index('Date') \
+                .groupby(pd.Grouper(freq=freq))['Price Target'].mean()
+        else:
+            print('Not implemented type of metric')
+            return 0
+
+        # series.index = series.index.map(lambda x: f'{x.year}-{x.month}')
+        res[f'{metric_type.upper()}_ANALYST_{t.upper()}'] = series
+    return res
+
+
+def computing(url):
+
+    # getting the table from web page
+    soup = get_soup(get_html(url))
+    df = pd.read_html(
+        str(soup.find('table', {'class': 'scroll-table sort-table'})))[0]
+
+    # table preprocessing: Date column to datetime format,
+    # empty fields in Price Target column to 00 string,
+    # formatting of Price Target string
+    df.Date = df['Date'].astype('datetime64')
+    df.Rating = df.Rating.apply(preprocess_rating_column)
+    df['Price Target'].fillna('00', inplace=True)
+    df['Price Target'] = df['Price Target'].apply(clean_price_target)
+
+    return {'ins': get_metrics(df, 'ins'),
+            'amt': get_metrics(df, 'amt')}
+
+
+if __name__ == '__main__':
+    url = ('https://www.marketbeat.com/stocks/'
+           'NASDAQ/MSFT/price-target/?MostRecent=0')
+
+    res = computing(url)
+
+    with pd.option_context('display.max_rows', None):
+        for metric_type in ['amt', 'ins']:
+            for key in res[metric_type]:
+                print('*' * 10)
+                print(f'***{key}***')
+                print(res[metric_type][key].dropna())
+
---
+++ tests.py
@@ -0,0 +1,37 @@
+import unittest
+from script import computing
+
+
+class TestScript(unittest.TestCase):
+    def test_computing(self):
+        url = ('https://www.marketbeat.com/stocks/'
+               'NASDAQ/MSFT/price-target/?MostRecent=0')
+        result = computing(url)
+
+        amt_buy_18_10_19 = 105
+        self.assertEqual(result['amt']['AMT_ANALYST_BUY']['2019-10-18'],
+                         amt_buy_18_10_19)
+
+        amt_buy_19_10_7 = 160
+        self.assertEqual(result['amt']['AMT_ANALYST_BUY']['2019-10-07'],
+                         amt_buy_19_10_7)
+
+        amt_sell_19_07_19 = 93
+        self.assertEqual(result['amt']['AMT_ANALYST_SELL']['2019-07-19'],
+                         amt_sell_19_07_19)
+
+        amt_hold_19_07_15 = 147
+        self.assertEqual(result['amt']['AMT_ANALYST_HOLD']['2019-07-15'],
+                         amt_hold_19_07_15)
+
+        ins_buy_20_2_27 = 4
+        self.assertEqual(result['ins']['INS_ANALYST_BUY']['2020-02-27'],
+                         ins_buy_20_2_27)
+
+        ins_sell_18_6_11 = 1
+        self.assertEqual(result['ins']['INS_ANALYST_SELL']['2018-06-11'],
+                         ins_sell_18_6_11)
+
+        ins_hold_19_7_15 = 1
+        self.assertEqual(result['ins']['INS_ANALYST_HOLD']['2019-07-15'],
+                         ins_hold_19_7_15)
